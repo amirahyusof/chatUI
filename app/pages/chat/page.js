062298a29271918dcdb0c533a43ee1d37632aec0
amirah <mirez.data@gmail.com>
2024-11-25 10:18:58 +0800

    add chat page

---
+++ app/pages/chat/page.js
@@ -0,0 +1,11 @@
+import Header from '@/components/navigation/header'
+import React from 'react'
+
+export default function ChatPage(){
+  return (
+    <section>
+      <Header />
+    </section>
+  )
+}
+
